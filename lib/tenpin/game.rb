7ea971ec85d3512fd15cf196128eaf3504637a12
Piotr Murach <pmurach@gmail.com>
2019-10-06 12:48:54 +0200

    Add game key events

--- lib/tenpin/game.rb
+++ lib/tenpin/game.rb
@@ -21,6 +21,35 @@ module Tenpin
       @cursor = TTY::Cursor
       @reader = TTY::Reader.new(interrupt: :exit)
       @rows, @cols = TTY::Screen.size
+
+      register_events
+    end
+
+    def register_events
+      @reader.on(:keypress) do |event|
+        if [?\C-x, 'q'].include?(event.value)
+          puts cursor.clear_screen
+          puts cursor.move_to(rows - 1, 0)
+          exit
+        end
+      end
+
+      input_handler = Thread.new do
+        Thread.abort_on_exception = true
+
+        loop do
+          char = @reader.read_keypress
+          if [?\C-x, 'q'].include?(char)
+            break
+          end
+        end
+      end
+
+      at_exit do
+        print TTY::Cursor.show
+        input_handler.kill
+        puts "Exiting Tenpin Bowling..."
+      end
     end
 
     # Start the game
