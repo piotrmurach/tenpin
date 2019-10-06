33e0e37bbf839a5c54fe381f51517b33d1267c92
Piotr Murach <pmurach@gmail.com>
2019-10-06 11:59:29 +0200

    Add debug helper

--- lib/tenpin.rb
+++ lib/tenpin.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require "tty-screen"
+require "tty-cursor"
+
 require_relative "tenpin/game"
 require_relative "tenpin/version"
 
@@ -13,5 +16,27 @@ module Tenpin
   def self.run
     game = Tenpin::Game.new
     game.run
+  end
+
+  # Print debug information in terminal top right corner
+  #
+  # @example
+  #   prompt.debug "info1", "info2"
+  #
+  # @param [Array] messages
+  #
+  # @retrun [nil]
+  #
+  # @api public
+  def self.debug(*messages)
+    longest = messages.max_by(&:length).size
+    width = TTY::Screen.width - longest
+    print TTY::Cursor.save
+    messages.each_with_index do |msg, i|
+      print TTY::Cursor.move_to(width, i)
+      print TTY::Cursor.clear_line_after
+      print msg
+    end
+    print TTY::Cursor.restore
   end
 end # Tenpin
