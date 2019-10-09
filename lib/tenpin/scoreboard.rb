16da265398a4a25c4ac397cdb4bb7d207c168944
Piotr Murach <pmurach@gmail.com>
2019-10-09 21:41:28 +0200

    Change to correctly dislay individual frame rolls with symbols for a strike, a spare or a miss

--- lib/tenpin/scoreboard.rb
+++ lib/tenpin/scoreboard.rb
@@ -5,20 +5,22 @@ require_relative "entity"
 module Tenpin
   class Scoreboard < Entity
 
-    def initialize(x, y, score: nil)
+    def initialize(x, y, scores: [])
       super(x, y)
 
-      @score = score
+      @scores = scores
     end
 
     # Draw a scoreboard with current scores
     #
     # @api public
     def draw(canvas = $stdout)
-      template.lines.each.with_index do |line, i|
+      template.lines.each_with_index do |line, i|
         canvas.print @cursor.move_to(pos.x, pos.y + i) + line
       end
     end
+
+    private
 
     # The score template
     #
@@ -51,9 +53,9 @@ module Tenpin
     def score_line(frames)
       frames.reduce([]) do |line, frame|
         if frame == 9
-          line << frame_throw1(frame) + "|" + frame_throw2(frame) + "|" + " "
+          line << "#{first_roll(frame)}|#{second_roll(frame)}|#{third_roll(frame)}"
         else
-          line << " " + frame_throw1(frame) + "|" + frame_throw2(frame)
+          line << " #{first_roll(frame)}|#{second_roll(frame)}"
         end
         line
       end
@@ -74,34 +76,86 @@ module Tenpin
       end
     end
 
+    # Marks 1st roll on the board
+    #
+    # @return [String]
+    #
     # @api private
-    def frame_throw1(frame)
-      scores = @score[frame]
-      scores.nil? ? "  " : scores[0].to_s.rjust(2)
-    end
-
-    # @api private
-    def frame_throw2(frame)
-      scores = @score[frame]
-      if scores.nil?
-        " "
-      elsif scores[0] == 10
-        "x"
-      elsif scores[0].to_i + scores[1].to_i == 10
-        "\\"
+    def first_roll(frame)
+      scores = @scores[frame]
+      if scores.nil? || strike?(frame)
+        "  "
+      elsif scores[0].to_i == 0
+        " -"
       else
-        scores[1].to_s.rjust(1)
+        scores[0].to_s.rjust(2)
       end
     end
 
+    # Marsk 2nd roll on the board
+    #
+    # Following symbols signify:
+    #
+    # x - a strike
+    # / - a spare
+    # - - no pins knocked down
+    #
+    # @return [String]
+    #
+    # @api private
+    def second_roll(frame)
+      scores = @scores[frame]
+      if scores.nil?
+        " "
+      elsif strike?(frame)
+        "x"
+      elsif spare?(frame)
+        "/"
+      elsif scores[1].to_i == 0
+        "-"
+      else
+        scores[1].to_s
+      end
+    end
+
+    # Mark 3rd roll on the board
+    #
+    # @api private
+    def third_roll(frame)
+      scores = @scores[frame]
+      scores.nil? ? " " : (scores[2] || " ")
+    end
+
+    # Check frame for strike
+    #
+    # @api private
+    def strike?(frame)
+      scores = @scores[frame]
+      return false if scores.nil?
+
+      scores[0].to_i == 10 || scores[1].to_i == 10
+    end
+
+    # Check frame for spare
+    #
+    # @api private
+    def spare?(frame)
+      scores = @scores[frame]
+      return false if scores.nil?
+
+      (scores[0].to_i != 10) && (scores[0].to_i + scores[1].to_i == 10)
+    end
+
+    # Display frame total
+    #
     # @api private
     def frame_total(frame)
       width = (frame == 9 ? 6 : 5)
-      if @score[frame].nil?
+      if @scores[frame].nil?
         return " " * width
       end
 
-      (@score[frame][0].to_i + @score[frame][1].to_i).to_s.rjust(width)
+      (@scores[frame][0].to_i + @scores[frame][1].to_i).to_s.rjust(width)
     end
   end # Scoreboard
 end # Tenpin
--- spec/unit/scoreboard/draw_spec.rb
+++ spec/unit/scoreboard/draw_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Tenpin::Scoreboard, "#draw" do
   let(:output) { StringIO.new }
 
   it "draws score board without scores" do
-    scoreboard = Tenpin::Scoreboard.new(0, 0, score: [])
+    scoreboard = Tenpin::Scoreboard.new(0, 0, scores: [])
 
     scoreboard.draw(output)
 
@@ -23,24 +23,26 @@ RSpec.describe Tenpin::Scoreboard, "#draw" do
   end
 
   it "draws score board with all scores" do
-    score = [
-      [3,4], [5,2], [3,7], [10,4], [2,2],
-      [4,2], [5,5], [2,6], [10,6], [4,4]
+    scores = [
+    # gutter        spare strike
+      [7,0], [5,2], [3,7], [10], [2,2],
+    #        spare  gutter  strike  spare
+      [4,2], [5,5], [0,6], [0,10], [6,4,3]
     ]
-    scoreboard = Tenpin::Scoreboard.new(0, 0, score: score)
+    scoreboard = Tenpin::Scoreboard.new(0, 0, scores: scores)
 
     scoreboard.draw(output)
 
     expect(output.string).to eq([
       "\e[1;1H _____________________________\n",
       "\e[2;1H|__1__|__2__|__3__|__4__|__5__|\n",
-      "\e[3;1H|  3|4|  5|2|  3|\\| 10|x|  2|2|\n",
-      "\e[4;1H|    7|    7|   10|   14|    4|\n",
+      "\e[3;1H|  7|-|  5|2|  3|/|   |x|  2|2|\n",
+      "\e[4;1H|    7|    7|   10|   10|    4|\n",
       "\e[5;1H|_____|_____|_____|_____|_____|\n",
       "\e[6;1H ______________________________\n",
       "\e[7;1H|__6__|__7__|__8__|__9__|__10__|\n",
-      "\e[8;1H|  4|2|  5|\\|  2|6| 10|x| 4|4| |\n",
-      "\e[9;1H|    6|   10|    8|   16|     8|\n",
+      "\e[8;1H|  4|2|  5|/|  -|6|   |x| 6|/|3|\n",
+      "\e[9;1H|    6|   10|    6|   10|    10|\n",
       "\e[10;1H|_____|_____|_____|_____|______|\n"
     ].join)
   end
